e9ffa21ff1eb99fc882a7f2c53701f6185f2b070
Otto <otto@ot-to.nl>
2019-05-13 13:14:34 +0200

    Case insensitive header names

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,5 +7,15 @@ fn main() {
     let handle = stdin.lock();
     let warc = WarcReader::new(handle);
 
-    println!("Records: {}", warc.count());
+    let mut response_counter = 0;
+    for item in warc {
+        let record = item.unwrap(); // could be IO/malformed error
+
+        // header names are case insensitive
+        if record.header.get(&"WARC-Type".into()) == Some(&"response".into()) {
+            response_counter += 1;
+        }
+    }
+
+    println!("# response records: {}", response_counter);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,10 @@
 //!
 //!     let mut response_counter = 0;
 //!     for item in warc {
-//!         let record = item.unwrap(); // could be IO/Malformed error
-//!         if record.header.get("WARC-Type") == Some(&String::from("response")) {
+//!         let record = item.expect("IO/malformed error");
+//!
+//!         // header names are case insensitive
+//!         if record.header.get(&"WARC-Type".into()) == Some(&"response".into()) {
 //!             response_counter += 1;
 //!         }
 //!     }
@@ -28,18 +30,92 @@ use std::collections::HashMap;
 use std::io::BufRead;
 
 // trim a string in place (no (re)allocations)
-fn ltrim(s: &mut String) {
+fn rtrim(s: &mut String) {
     s.truncate(s.trim_end().len());
+}
+
+/// Case insensitive string
+///
+/// ```
+/// use rust_warc::CaseString;
+///
+/// // explicit constructor
+/// let s1 = CaseString::from(String::from("HELLO!"));
+///
+/// // implicit conversion from String or &str
+/// let s2: CaseString = "hello!".into();
+///
+/// assert_eq!(s1, s2);
+/// ```
+#[derive(PartialEq, Eq, Hash, Debug)]
+pub struct CaseString {
+    inner: String,
+}
+impl CaseString {
+    pub fn to_string(self) -> String {
+        self.into()
+    }
+}
+
+impl PartialEq<String> for CaseString {
+    fn eq(&self, other: &String) -> bool {
+        self.inner == other.to_ascii_lowercase()
+    }
+}
+
+impl From<String> for CaseString {
+    fn from(mut s: String) -> Self {
+        s.make_ascii_lowercase();
+
+        CaseString { inner: s }
+    }
+}
+impl From<&str> for CaseString {
+    fn from(s: &str) -> Self {
+        String::from(s).into()
+    }
+}
+
+impl Into<String> for CaseString {
+    fn into(self) -> String {
+        self.inner
+    }
 }
 
 /// WARC Record
 ///
 /// A record consists of the version string, a list of headers and the actual content (in bytes)
+///
+/// # Usage
+/// ```rust
+/// use rust_warc::WarcReader;
+///
+/// /* test.warc:
+/// WARC/1.1
+/// WARC-Type: warcinfo
+/// WARC-Date: 2006-09-19T17:20:14Z
+/// WARC-Record-ID: <urn:uuid:d7ae5c10-e6b3-4d27-967d-34780c58ba39>
+/// Content-Type: text/plain
+/// Content-Length: 4
+///
+/// test
+///
+/// */
+///
+/// let data = &include_bytes!("test.warc")[..];
+/// let mut warc = WarcReader::new(data);
+///
+/// let item = warc.next().expect("finished").expect("parse error");
+///
+/// assert_eq!(item.version, "WARC/1.1");
+/// assert_eq!(item.header.get(&"content-type".into()).unwrap(), "text/plain");
+/// assert_eq!(item.content, "test".as_bytes());
+/// ```
 pub struct WarcRecord {
     /// WARC version string (WARC/1.1)
     pub version: String,
     /// Record header fields
-    pub header: HashMap<String, String>,
+    pub header: HashMap<CaseString, String>,
     /// Record content block
     pub content: Vec<u8>,
 }
@@ -54,6 +130,20 @@ pub enum WarcError {
 /// WARC reader instance
 ///
 /// The WarcReader serves as an iterator for [WarcRecords](WarcRecord) (or [errors](WarcError))
+///
+/// # Usage
+/// ```rust
+/// use rust_warc::{WarcReader, WarcRecord, WarcError};
+///
+/// let data = &include_bytes!("warc.in")[..];
+/// let mut warc = WarcReader::new(data);
+///
+/// let item: Option<Result<WarcRecord, WarcError>> = warc.next();
+/// assert!(item.is_some());
+///
+/// // count remaining items
+/// assert_eq!(warc.count(), 2);
+/// ```
 pub struct WarcReader<R> {
     read: R,
     valid_state: bool,
@@ -90,7 +180,7 @@ impl<R: BufRead> Iterator for WarcReader<R> {
             return None; // EOF
         }
 
-        ltrim(&mut version);
+        rtrim(&mut version);
 
         if !version.starts_with("WARC/1.") {
             self.valid_state = false;
@@ -99,7 +189,7 @@ impl<R: BufRead> Iterator for WarcReader<R> {
             ))));
         }
 
-        let mut header = HashMap::with_capacity(16); // no allocations if <= 16 header fields
+        let mut header = HashMap::<CaseString, String>::with_capacity(16); // no allocations if <= 16 header fields
 
         loop {
             let mut line_buf = String::new();
@@ -109,20 +199,21 @@ impl<R: BufRead> Iterator for WarcReader<R> {
                 return Some(Err(WarcError::IO(io)));
             }
 
-            if &line_buf == "\r\n" {
+            // leniency: allow absent carriage return
+            if &line_buf == "\r\n" || &line_buf == "\n" {
                 break;
             }
 
             // todo field multiline continuations
 
-            ltrim(&mut line_buf);
+            rtrim(&mut line_buf);
 
             if let Some(semi) = line_buf.find(':') {
                 let value = line_buf.split_off(semi + 1).trim().to_string();
                 line_buf.pop(); // eat colon
-                ltrim(&mut line_buf);
+                rtrim(&mut line_buf);
 
-                header.insert(line_buf, value);
+                header.insert(line_buf.into(), value);
             } else {
                 self.valid_state = false;
                 return Some(Err(WarcError::Malformed(String::from(
@@ -131,7 +222,7 @@ impl<R: BufRead> Iterator for WarcReader<R> {
             }
         }
 
-        let content_len = header.get("Content-Length");
+        let content_len = header.get(&"Content-Length".into());
         if content_len.is_none() {
             self.valid_state = false;
             return Some(Err(WarcError::Malformed(String::from(
@@ -191,14 +282,20 @@ mod tests {
         let item = item.unwrap();
         assert!(item.is_ok());
         let item = item.unwrap();
-        assert_eq!(item.header.get("WARC-Type"), Some(&"warcinfo".to_string()));
+        assert_eq!(
+            item.header.get(&"WARC-Type".into()),
+            Some(&"warcinfo".into())
+        );
 
         let item = warc.next();
         assert!(item.is_some());
         let item = item.unwrap();
         assert!(item.is_ok());
         let item = item.unwrap();
-        assert_eq!(item.header.get("WARC-Type"), Some(&"request".to_string()));
+        assert_eq!(
+            item.header.get(&"WARC-Type".into()),
+            Some(&"request".into())
+        );
 
         let item = warc.next();
         assert!(item.is_some());
